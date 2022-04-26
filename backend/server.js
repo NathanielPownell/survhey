3f07989a1afe485bf75e33c8ddd39e37a2997d3b
NATHANIEL POWNELL <nathanieldpownell@gmail.com>
2022-04-26 06:51:45 -0400

    remove imports from express

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
-const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -19,13 +18,18 @@ app.use('/api/questions', require('./routes/questionRoutes'))
 app.use('/api/answers', require('./routes/answerRoutes'))
 
 // Serve frontend
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
 } else {
-    app.get('/', (req, res) => res.send('Set to production.'))
+    app.get('/', (req, res) => res.send('Please set to production'))
 }
+
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
