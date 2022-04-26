1e47faac729ecf1f99338180ec67c549a6df70d2
NATHANIEL POWNELL <nathanieldpownell@gmail.com>
2022-04-26 07:04:17 -0400

    change server.js to mern-proj structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store, persistor } from './app/store';
+import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import { PersistGate } from 'redux-persist/integration/react'
-import Spinner from './components/Spinner';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={<Spinner />} persistor={persistor}> */}
         <App />
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
